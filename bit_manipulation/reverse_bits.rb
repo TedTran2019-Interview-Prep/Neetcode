0fef9a785a81d289376c09efdea89aba47cd1527
Ted Tran <tedtran2019@gmail.com>
2022-05-11 22:21:52 -0700

    Added a cleaner solution to reverse bits

--- bit_manipulation/reverse_bits.rb
+++ bit_manipulation/reverse_bits.rb
@@ -10,6 +10,7 @@ def reverse_bits(n)
     n.to_s(2).rjust(32, '0').reverse.to_i(2)
 end
 
+# Can do 31.downto(0)
 def reverse_bits(n)
   reversed_bits = 0
   bit = 31
@@ -18,6 +19,16 @@ def reverse_bits(n)
     reversed_bits = reversed_bits | insert
     bit -= 1
     n = n >> 1
+  end
+  reversed_bits
+end
+
+# Can do 31.downto(0) for cleaner code
+def reverse_bits(n)
+  reversed_bits = 0
+  31.downto(0) do |nbr|
+    insert = ((n >> (31 - nbr)) & 1) << nbr
+    reversed_bits = reversed_bits | insert
   end
   reversed_bits
 end
