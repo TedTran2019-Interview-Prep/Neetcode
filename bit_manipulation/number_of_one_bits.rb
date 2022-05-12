bcd7dec01ddfc4067a2f911ba9e3577dce418a80
Ted Tran <tedtran2019@gmail.com>
2022-05-11 17:04:18 -0700

    Looked at other solutions for number of one bits

--- bit_manipulation/number_of_one_bits.rb
+++ bit_manipulation/number_of_one_bits.rb
@@ -4,3 +4,25 @@
 def hamming_weight(n)
   n.to_s(2).count('1');
 end
+
+# Utilizing modulo
+# Last number decides if it's odd or not
+# Bit is activated == odd == 1, bit is not activated == even == 0
+def hamming_weight(n)
+  total_ones = 0
+  until n.zero?
+    total_ones += (n % 2)
+    n = n >> 1
+  end
+  total_ones
+end
+
+# Utilizing &
+def hamming_weight(n)
+  total_ones = 0
+  until n.zero?
+    n = n & (n - 1)
+    total_ones += 1
+  end
+  total_ones
+end
