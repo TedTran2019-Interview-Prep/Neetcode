a2e41ba12ae55b5dff38250dd10f896fd35442e6
Ted Tran <tedtran2019@gmail.com>
2022-05-12 13:23:13 -0700

    Completed missing number

---
+++ bit_manipulation/missing_number.rb
@@ -0,0 +1,23 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def missing_number(nums)
+  nbr1 = nums.inject { |accu, n| accu ^ n }
+  nbr2 = (0..nums.length).inject { |accu, n| accu ^ n }
+  nbr1 ^ nbr2
+end
+
+def missing_number(nums)
+  missing = 0
+  (0..nums.length).each do |i|
+    nbr = (nums[i].nil? ? 0 : nums[i])
+    missing += (i - nbr)
+  end
+  missing
+end
+
+# Gauss summation of numbers
+def missing_number(nums)
+  gauss = (nums.length / 2.0) * (nums.length + 1)
+  nums.each { |n| gauss -= n }
+  gauss.to_i
+end
