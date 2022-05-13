c8e951f67b0779114f8d81818949e3ea1daa7840
Ted Tran <tedtran2019@gmail.com>
2022-05-13 11:59:36 -0700

    Added optimization for top k frequent elements. Starting on product of array except self now

---
+++ arrays_hashing/product_of_array_except_self.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @return {Integer[]}
+# answer[i] = rest of elements multiplied together, except self
+# If division were allowed, can just run through array twice.
+# Once to get total multiplication, then divide total by self for each element.
+# 
+def product_except_self(nums)
+    
+end
--- arrays_hashing/top_k_frequent_elements.rb
+++ arrays_hashing/top_k_frequent_elements.rb
@@ -7,8 +7,9 @@ def top_k_frequent(nums, k)
   hash.to_a.sort { |a, b| b[1] <=> a[1] }.take(k).map { |k_v| k_v[0] }
 end
 
-# Bucket sort solution (Clever as hell, thanks Neetcode)
+# O(N) (Clever as hell, thanks Neetcode)
 # Maximum number of occurrences == nums.length
+# Can be optimized further by keeping a max_freq variable to bound the counts array
 def top_k_frequent(nums, k)
   hash = Hash.new(0)
   counts = Array.new(nums.length + 1) { [] }
@@ -16,6 +17,26 @@ def top_k_frequent(nums, k)
   hash.each { |k, v| counts[v] << k }
   top_k = []
   nums.length.downto(1) do |n|
+    return top_k if k <= 0
+    next if counts[n].empty?
+
+    top_k.concat(counts[n].take(k))
+    k -= counts[n].length
+  end
+  top_k
+end
+
+def top_k_frequent(nums, k)
+  hash = Hash.new(0)
+  max_freq = 0
+  nums.each do |num| 
+    hash[num] += 1 
+    max_freq = hash[num] if hash[num] > max_freq
+  end
+  counts = Array.new(max_freq + 1) { [] }
+  hash.each { |k, v| counts[v] << k }
+  top_k = []
+  max_freq.downto(1) do |n|
     return top_k if k <= 0
     next if counts[n].empty?
 
