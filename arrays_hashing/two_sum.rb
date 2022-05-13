ccbd0f16670bb6b8f423e683981afabb511fb794
Ted Tran <tedtran2019@gmail.com>
2022-05-13 10:32:02 -0700

    Completed two sum

---
+++ arrays_hashing/two_sum.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+  hash = {}
+  nums.each_with_index do |num, idx|
+    return [hash[num], idx] if hash.key? num
+
+    hash[target - num] = idx
+  end
+  nil
+end
