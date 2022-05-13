4cd49d06881720955c7ff85d06a503afdd4c29f5
Ted Tran <tedtran2019@gmail.com>
2022-05-13 10:15:22 -0700

    Easy question

---
+++ arrays_hashing/contains_duplicate.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums
+# @return {Boolean}
+# Can also be done by converting to a set and comparing lengths
+def contains_duplicate(nums)
+  hash = {}
+  nums.each do |num|
+    return true if hash.key? num
+
+    hash[num] = true
+  end
+  false
+end
