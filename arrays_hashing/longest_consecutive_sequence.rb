c662d9adacf9d3c841e82efe38e463130e96d98c
Ted Tran <tedtran2019@gmail.com>
2022-05-13 15:57:34 -0700

    Completed another solution for consecutive longest sequence

--- arrays_hashing/longest_consecutive_sequence.rb
+++ arrays_hashing/longest_consecutive_sequence.rb
@@ -4,7 +4,7 @@
 # -2 == earlier point has read
 def longest_consecutive(nums)
   return 0 if nums.empty?
-  
+
   hash = {}
   nums.each { |num| hash[num] = -1 }
   nums.each do |num|
@@ -35,3 +35,30 @@ nums = [100,4,200,1,3,2]
 puts longest_consecutive(nums)
 nums = [0,3,7,2,5,8,4,6,0,1]
 puts longest_consecutive(nums)
+
+# Neetcode's solution is a lot better
+# E.g, check for left neighbor to see if it's the start of a sequence
+# If it's the start of a sequence, then calculate length
+# Then choose longest length
+def longest_consecutive(nums)
+  return 0 if nums.empty?
+
+  hash = {}
+  nums.each { |num| hash[num] = true }
+  longest = 0
+  nums.each do |num|
+    next if hash[num - 1] 
+
+    challenger = 1
+    while true
+      if hash[num + challenger]
+        challenger += 1
+      else
+        break
+      end
+    end
+    longest = challenger if challenger > longest
+  end
+
+  longest
+end
