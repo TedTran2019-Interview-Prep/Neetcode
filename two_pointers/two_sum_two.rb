da008b271a85de92417c9646bac852ffd4fd0f78
Ted Tran <tedtran2019@gmail.com>
2022-05-13 17:27:42 -0700

    This one is super easy, surprised it was a medium

---
+++ two_pointers/two_sum_two.rb
@@ -0,0 +1,31 @@
+# @param {Integer[]} numbers
+# @param {Integer} target
+# @return {Integer[]}
+# Array is already sorted
+def two_sum(numbers, target)
+  idx_start = 0
+  idx_end = numbers.length - 1
+  while idx_start < idx_end
+    case numbers[idx_start] + numbers[idx_end] <=> target
+    when 1
+      idx_end -= 1
+    when 0
+      return [idx_start + 1, idx_end + 1]
+    when -1
+      idx_start += 1
+    end
+  end
+  nil
+end
+
+numbers = [2,7,11,15]
+target = 9
+print two_sum(numbers, target)
+
+numbers = [2,3,4]
+target = 6
+print two_sum(numbers, target)
+
+numbers = [-1,0]
+target = -1
+print two_sum(numbers, target)
