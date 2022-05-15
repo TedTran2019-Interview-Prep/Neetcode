214cccd2dc61a595d61bc8707e39874aa8b9ba67
Ted Tran <tedtran2019@gmail.com>
2022-05-14 23:32:15 -0700

    Completed max depth of binary tree

---
+++ trees/max_depth_of_binary_tree.rb
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def max_depth(root)
+  return 0 if root.nil?
+  return 1 if root.left.nil? && root.right.nil?
+
+  left =  1 + max_depth(root.left)
+  right = 1 + max_depth(root.right)
+  left > right ? left : right
+end
