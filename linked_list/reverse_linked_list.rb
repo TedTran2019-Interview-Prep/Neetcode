7d57c32faaa7b34700ec0cf2bcfb6a29a03e8c9e
Ted Tran <tedtran2019@gmail.com>
2022-05-14 22:05:43 -0700

    Completed reverse linked list

---
+++ linked_list/reverse_linked_list.rb
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_list(head)
+  next_node = nil
+  prev_node = nil
+  while true
+    next_node = head.next
+    head.next = prev_node
+    prev_node = head
+    if next_node.nil? 
+      return head
+    else
+      head = next_node
+    end
+  end
+end
+
+def reverse_list(head)
+  return head if head.nil?
+
+  newHead = head
+  if head.next
+    newHead = reverse_list(head.next)
+    head.next.next = head
+  end
+
+  head.next = nil
+  return newHead
+end
